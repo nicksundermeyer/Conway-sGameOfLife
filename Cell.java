985b91d3be0c1c7ae4a79f4007a9b81cf4a840ca
nicksundermeyer <nicksundermeyer@gmail.com>
2013-11-21 18:12:04 -0800

    First commit

---
+++ Cell.java
@@ -0,0 +1,264 @@
+import java.awt.Color;
+import java.awt.Graphics;
+
+public class Cell {
+	private int myX, myY; // x,y position on grid
+	private boolean myAlive; // alive (true) or dead (false)
+	private int myNeighbors; // count of neighbors with respect to x,y
+	private boolean myAliveNextTurn; // Used for state in next iteration
+	private Color myColor; // Based on alive/dead rules
+	private final Color DEFAULT_ALIVE = Color.ORANGE;
+	private final Color DEFAULT_DEAD = Color.GRAY;
+
+	public Cell(int x, int y) {
+		this(x, y, false, Color.GRAY);
+	}
+
+	public Cell(int row, int col, boolean alive, Color color) {
+		myAlive = alive;
+		myColor = color;
+		myX = col;
+		myY = row;
+	}
+
+	public boolean getAlive() {
+		return myAlive;
+	}
+
+	public int getX() {
+		return myX;
+	}
+
+	public int getY() {
+		return myY;
+	}
+
+	public Color getColor() {
+		return myColor;
+	}
+
+	public void setAlive(boolean alive) {
+		if (alive) {
+			setAlive(true, DEFAULT_ALIVE);
+		} else {
+			setAlive(false, DEFAULT_DEAD);
+		}
+	}
+
+	public void setAlive(boolean alive, Color color) {
+		myColor = color;
+		myAlive = alive;
+	}
+
+	public void setAliveNextTurn(boolean alive) {
+		myAliveNextTurn = alive;
+	}
+
+	public boolean getAliveNextTurn() {
+		return myAliveNextTurn;
+	}
+
+	public void setColor(Color color) {
+		myColor = color;
+	}
+
+	public int getNeighbors() {
+		return myNeighbors;
+	}
+
+	public void calcNeighbors(Cell[][] cell) {
+	/* The method calcNeighbors calculates the number of alive neighbors 
+	 * 
+	 */
+		myNeighbors = 0;
+		int row = this.getY();
+		int col = this.getX();
+		
+		boolean myLeftNeighborIsAlive = false;
+		
+		//What if col if zero?
+		
+		if (row - 1 > 0 && col - 1 > 0 && row + 1 < Display.ROWS && col + 1 < Display.COLS) {
+		Cell myLeftNeighbor = cell[row][col - 1];
+		Cell myRightNeighbor = cell[row][col + 1];
+		
+		Cell myTopNeighbor = cell[row + 1][col];
+		Cell myBottomNeighbor = cell[row - 1][col];
+		
+		Cell myTopRightNeighbor = cell[row + 1][col + 1];
+		Cell myBottomRightNeighbor = cell[row -1][col + 1];
+		
+		Cell myTopLeftNeighbor = cell[row + 1][col -1];
+		Cell myBottomLeftNeighbor = cell[row -1][col-1];
+		
+		if (row == 0) myTopNeighbor = cell[79][col];
+		if (row == 99) myBottomNeighbor = cell[0][col];
+		if (col == 0) myLeftNeighbor = cell[row][99];
+		if (col == 99) myRightNeighbor = cell[row][0];
+		
+		if (row == 0) myTopLeftNeighbor = cell[79][col-1];
+		if (row == 99) myBottomLeftNeighbor = cell[0][col-1];
+		if (row == 0) myTopRightNeighbor = cell[79][col+1];
+		if (row == 99) myBottomRightNeighbor = cell[0][col+1];
+		
+		if (col == 0) myTopLeftNeighbor = cell[row-1][99];
+		if (col == 99) myTopRightNeighbor = cell[row-1][0];
+		if (col == 0) myBottomLeftNeighbor = cell[row+1][99];
+		if (col == 99) myBottomRightNeighbor = cell[row+1][0];
+		
+		
+		if (col == 0) myTopRightNeighbor = cell[row][99];
+		if (col == 99) myRightNeighbor = cell[row][0];
+
+/*
+		this code doesn't work because nothing has been written to do the diagonal cases
+		because of this, the code gets an error when it attempts to test the cases for the corners of the grid
+		if we can't find another simpler way to do it, this will work, but i'd rather have something else that is simpler if possible
+		*/
+
+
+/*
+		else if (col==0){
+			myLeftNeighbor = cell[row][Display.COLS - 1];
+			myRightNeighbor = cell[row][col + 1];
+			
+			myTopNeighbor = cell[row + 1][col];
+			myBottomNeighbor = cell[row - 1][col];
+			
+			myTopRightNeighbor = cell[row + 1][col + 1];
+			myBottomRightNeighbor = cell[row -1][col + 1];
+			
+			myTopLeftNeighbor = cell[row + 1][Display.COLS - 1];
+			myBottomLeftNeighbor = cell[row -1][Display.COLS - 1];
+		}
+		else if (row==0){
+			myLeftNeighbor = cell[row][col - 1];
+			myRightNeighbor = cell[row][col + 1];
+			
+			myTopNeighbor = cell[Display.ROWS-1][col];
+			myBottomNeighbor = cell[row - 1][col];
+			
+			myTopRightNeighbor = cell[Display.ROWS-1][col + 1];
+			myBottomRightNeighbor = cell[Display.ROWS-1][col + 1];
+			
+			myTopLeftNeighbor = cell[row + 1][col -1];
+			myBottomLeftNeighbor = cell[row -1][col-1];
+		}
+		else if (col==Display.COLS){
+			myLeftNeighbor = cell[row][col - 1];
+			myRightNeighbor = cell[row][0];
+			
+			myTopNeighbor = cell[row + 1][col];
+			myBottomNeighbor = cell[row - 1][col];
+			
+			myTopRightNeighbor = cell[row + 1][0];
+			myBottomRightNeighbor = cell[row -1][0];
+			
+			myTopLeftNeighbor = cell[row + 1][col -1];
+			myBottomLeftNeighbor = cell[row -1][col-1];
+		}
+		else if (row==Display.ROWS){
+			myLeftNeighbor = cell[row][col - 1];
+			myRightNeighbor = cell[row][col + 1];
+			
+			myTopNeighbor = cell[row + 1][col];
+			myBottomNeighbor = cell[0][col];
+			
+			myTopRightNeighbor = cell[row + 1][col + 1];
+			myBottomRightNeighbor = cell[0][col + 1];
+			
+			myTopLeftNeighbor = cell[row + 1][col -1];
+			myBottomLeftNeighbor = cell[0][col-1];
+		}
+		*/
+
+		
+		//Now to count the number of cells that are alive.
+		
+		if (myLeftNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myRightNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myTopNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myBottomNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myTopRightNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myBottomRightNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myTopLeftNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		
+		if (myBottomLeftNeighbor.getAlive()) {
+			myNeighbors++;
+		}
+		}
+
+	}
+
+	public void willIBeAliveNextTurn() {
+		
+		if (this.getAlive()) {
+			
+			/* with fewer than two live neighbors dies, as if caused by under-population
+			 * with two or three live neighbors lives on to the next generation.
+			 * with more than three live neighbors dies, as if by overcrowding
+			 */
+			
+			if (this.getNeighbors() < 2) {
+				this.setAliveNextTurn(false);
+			}
+			
+			if (this.getNeighbors() == 2 || this.getNeighbors() == 3) {
+				this.setAliveNextTurn(true);
+			}
+			
+			if (this.getNeighbors() > 3) {
+				this.setAliveNextTurn(false);
+			}
+			
+		}
+		else {
+		/* 
+		 * with exactly three live neighbors becomes a live cell, as if by reproducing
+		 */
+			if (this.getNeighbors() == 3) {
+				this.setAliveNextTurn(true);
+			}
+			else {
+				this.setAliveNextTurn(false);
+			}
+		 
+		}
+		
+	}
+
+	public void draw(int x_offset, int y_offset, int width, int height,
+			Graphics g) {
+		// I leave this understanding to the reader
+		int xleft = x_offset + 1 + (myX * (width + 1));
+		int xright = x_offset + width + (myX * (width + 1));
+		int ytop = y_offset + 1 + (myY * (height + 1));
+		int ybottom = y_offset + height + (myY * (height + 1));
+		Color temp = g.getColor();
+
+		g.setColor(myColor);
+		g.fillRect(xleft, ytop, width, height);
+	}
+}
+
+
---
+++ Display.java
@@ -0,0 +1,348 @@
+import java.awt.Color;
+import java.awt.Graphics;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+import javax.swing.JButton;
+import javax.swing.JComponent;
+
+// Note that the JComponent is set up to listen for mouse clicks
+// and mouse movement.  To achieve this, the MouseListener and
+// MousMotionListener interfaces are implemented and there is additional
+// code in init() to attach those interfaces to the JComponent.
+
+
+public class Display extends JComponent implements MouseListener, MouseMotionListener {
+	public static final int ROWS = 80;
+	public static final int COLS = 100;
+	public static Cell[][] cell = new Cell[ROWS][COLS];
+	private final int X_GRID_OFFSET = 25; // 25 pixels from left
+	private final int Y_GRID_OFFSET = 40; // 40 pixels from top
+	private final int CELL_WIDTH = 5;
+	private final int CELL_HEIGHT = 5;
+
+	// Note that a final field can be initialized in constructor
+	private final int DISPLAY_WIDTH;   
+	private final int DISPLAY_HEIGHT;
+	private StartButton startStop;
+	private StepButton step;
+	private ClearButton clear;
+	private ChooseButton choose;
+	private ExitButton exit;
+	private boolean paintloop = false;
+
+
+	public Display(int width, int height) {
+		DISPLAY_WIDTH = width;
+		DISPLAY_HEIGHT = height;
+		init();
+	}
+
+
+	public void init() {
+		setSize(DISPLAY_WIDTH, DISPLAY_HEIGHT);
+		initCells();
+
+		addMouseListener(this);
+		addMouseMotionListener(this);
+
+		// Example of setting up a button.
+		// See the StartButton class nested below.
+		startStop = new StartButton();
+		startStop.setBounds(100, 550, 100, 36);
+		add(startStop);
+		startStop.setVisible(true);
+		repaint();
+		
+		step = new StepButton();
+		step.setBounds(205, 550, 100, 36);
+		add(step);
+		step.setVisible(true);
+		
+		clear = new ClearButton();
+		clear.setBounds(310, 550, 100, 36);
+		add(clear);
+		clear.setVisible(true);
+		
+		choose = new ChooseButton();
+		choose.setBounds(520, 550, 100, 36);
+		add(choose);
+		choose.setVisible(true);
+		
+		exit = new ExitButton();
+		exit.setBounds(415, 550, 100, 36);
+		add(exit);
+		exit.setVisible(true);
+	}
+
+
+	public void paintComponent(Graphics g) {
+		int TIME_BETWEEN_REPLOTS = 1000; // change to your liking
+
+		g.setColor(Color.BLACK);
+		drawGrid(g);
+		drawCells(g);
+		drawButtons();
+
+		if (paintloop) {
+			try {
+				Thread.sleep(TIME_BETWEEN_REPLOTS);
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			nextGeneration();
+			repaint();
+		}
+		
+		repaint();
+	}
+
+
+	public void initCells() {
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLS; col++) {
+				cell[row][col] = new Cell(row, col);
+			}
+		}
+		
+		cell[36][22].setAlive(true); // sample use of cell mutator method
+		cell[36][23].setAlive(true); // sample use of cell mutator method
+		cell[36][24].setAlive(true); // sample use of cell mutator method
+		cell[25][25].setAlive(true); 
+		cell[26][25].setAlive(true);
+		cell[37][25].setAlive(true);
+		cell[42][25].setAlive(true);
+		cell[41][26].setAlive(true);
+		cell[45][24].setAlive(true);
+		cell[56][24].setAlive(true);
+		cell[44][25].setAlive(true);
+		
+		
+	}
+
+	
+
+	public void togglePaintLoop() {
+		paintloop = !paintloop;
+	}
+
+
+	public void setPaintLoop(boolean value) {
+		paintloop = value;
+	}
+
+
+	void drawGrid(Graphics g) {
+		for (int row = 0; row <= ROWS; row++) {
+			g.drawLine(X_GRID_OFFSET,
+					Y_GRID_OFFSET + (row * (CELL_HEIGHT + 1)), X_GRID_OFFSET
+					+ COLS * (CELL_WIDTH + 1), Y_GRID_OFFSET
+					+ (row * (CELL_HEIGHT + 1)));
+		}
+		for (int col = 0; col <= COLS; col++) {
+			g.drawLine(X_GRID_OFFSET + (col * (CELL_WIDTH + 1)), Y_GRID_OFFSET,
+					X_GRID_OFFSET + (col * (CELL_WIDTH + 1)), Y_GRID_OFFSET
+					+ ROWS * (CELL_HEIGHT + 1));
+		}
+	}
+
+	
+	void drawCells(Graphics g) {
+		// Have each cell draw itself
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLS; col++) {
+				// The cell cannot know for certain the offsets nor the height
+				// and width; it has been set up to know its own position, so
+				// that need not be passed as an argument to the draw method
+				cell[row][col].draw(X_GRID_OFFSET, Y_GRID_OFFSET, CELL_WIDTH,
+						CELL_HEIGHT, g);
+			}
+		}
+	}
+
+
+	private void drawButtons() {
+		startStop.repaint();
+	}
+
+
+	private void nextGeneration() {
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLS; col++) {
+				cell[row][col].calcNeighbors(cell);
+				cell[row][col].willIBeAliveNextTurn();
+			}
+		}
+		
+		for (int row = 0; row < ROWS; row++) {
+			for (int col = 0; col < COLS; col++) {
+			boolean resultOfWillIBeAlive = cell[row][col].getAliveNextTurn();
+				cell[row][col].setAlive(resultOfWillIBeAlive);
+			}
+		}
+	}
+
+
+	public void mouseClicked(MouseEvent arg0) {
+		
+		
+		
+	}
+
+
+	public void mouseEntered(MouseEvent arg0) {
+	
+	}
+
+
+	public void mouseExited(MouseEvent arg0) {
+
+	}
+
+
+	public void mousePressed(MouseEvent arg0) {
+		
+	}
+
+
+	public void mouseReleased(MouseEvent arg0) {
+		
+	}
+
+
+	public void mouseDragged(MouseEvent arg0) {
+		int xCoordinate = (arg0.getX() - 25)/6;
+		int yCoordinate = (arg0.getY() - 40)/6;		
+		
+		if (cell[yCoordinate][xCoordinate].getAlive() == false) {
+			cell[yCoordinate][xCoordinate].setAlive(true);
+		}
+		
+	//	if (cell[yCoordinate][xCoordinate].
+	
+	}
+
+
+	public void mouseMoved(MouseEvent arg0) {
+		//DOES NOTHING
+	}
+	
+	private class ChooseButton extends JButton implements ActionListener {
+		ChooseButton() {
+			super("Alive");
+			addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			if (this.getText().equals("Alive")) {
+				setText("Dead");
+			} else if (this.getText().equals("Dead")){
+				setText("Alive");
+			}
+			repaint();
+		}
+	}
+	
+	private class ClearButton extends JButton implements ActionListener {
+		ClearButton() {
+			super("Clear");
+			addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			// nextGeneration(); // test the clear button
+			for (int row = 0; row < ROWS; row++) {
+				for (int col = 0; col < COLS; col++) {
+					cell[row][col].setAlive(false);
+				}
+			}
+			
+			repaint();
+		}
+	}
+	
+	private class SpeedButton extends JButton implements ActionListener {
+		SpeedButton() {
+			super("Slow");
+			addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			 nextGeneration(); // test the start button
+			if (this.getText().equals("Slow")) {
+				setText("Medium");
+			} else if (this.getText().equals("Medium")){
+
+				setText("Fast");
+			}
+			else if (this.getText().equals("Fast")) {
+				setText("Slow");
+			}
+			repaint();
+		}
+	}
+	
+	private class ExitButton extends JButton implements ActionListener {
+		ExitButton() {
+			super("Exit");
+			addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			 nextGeneration(); // test the start button
+			if (this.getText().equals("Exit")) {
+				togglePaintLoop();
+				setText("Exit");
+			} else if (this.getText().equals("Exit")){
+				togglePaintLoop();
+				setText("Exit");
+			}
+			repaint();
+		}
+	}
+	
+	private class StepButton extends JButton implements ActionListener {
+		StepButton() {
+			super("Step");
+			addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			nextGeneration();
+			
+			if (this.getText().equals("Step")) {
+				nextGeneration();
+				setText("Step");
+			} else {
+				nextGeneration();
+				setText("Step");
+			}
+			repaint();
+		}
+	}
+	
+	private class StartButton extends JButton implements ActionListener {
+		StartButton() {
+			super("Start");
+			addActionListener(this);
+		}
+
+		public void actionPerformed(ActionEvent arg0) {
+			 nextGeneration(); // test the start button
+			if (this.getText().equals("Start")) {
+				togglePaintLoop();
+				setText("Pause");
+			} else if (this.getText().equals("Pause")){
+				togglePaintLoop();
+				setText("Start");
+			}
+			repaint();
+		}
+	}
+}
+
+
+
